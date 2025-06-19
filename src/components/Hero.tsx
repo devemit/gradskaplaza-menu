1ee11d26a6c6124ecc99c276344ff6076214c628
Mitko <mitkoiliev505@gmail.com>
2025-06-19 23:20:09 +0200

    build fix

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,13 +1,9 @@
 import Menu from '../pages/MenuComponent/Menu';
-import Capacity from './CapacityComponent/Capacity';
-import Events from './EventComponent/Events';
 
 export default function Hero() {
   return (
     <>
       <Menu />
-      <Capacity />
-      <Events />
     </>
   );
 }
