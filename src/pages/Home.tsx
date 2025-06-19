7bf07ba557d39865d9c45d4f8d6c887219e979f7
Mitko <mitkoiliev505@gmail.com>
2025-06-19 23:10:34 +0200

    init

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,9 @@
 import Menu from './MenuComponent/Menu';
-import Capacity from '../components/CapacityComponent/Capacity';
-import Events from '../components/EventComponent/Events';
+
 export default function Home() {
   return (
     <div>
       <Menu />
-      <Capacity />
-      <Events />
     </div>
   );
 }
